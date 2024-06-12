0f70b79bd3d26e5dcb37265abe88d229f71a4fca
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-06-12 14:31:36 +0100

    src/index.ts - bind essential MM functions to the window object
    And use them in the respective page's script tags. This allows one MM
    bundle to be used to create two or more maps in separate pages.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { webRun } from "mykomap/index";
 import { config } from "./config";
 import { config2 } from "./config2";
 
-webRun(window, config);
-webRun(window, config2);
+// This puts the core Mykomap functions into the scope of a web page.
+// A work around - as mykomap not currently built by webpack as a EJS library
+// which can be imported into scripts.
+// @ts-ignore
+window.mykomapExports = { webRun, config, config2 };
